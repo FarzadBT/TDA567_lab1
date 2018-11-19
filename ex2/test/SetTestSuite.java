4b6b5b797c0daf910c92b28862bf28680344a994
FarzadBT <farmerfaz@gmail.com>
2018-11-19 15:36:12 +0100

    Added base for ex2

---
+++ ex2/test/SetTestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        //1st_test_class.class,
+        //...
+        //nth_test_class.class
+        })
+public class SetTestSuite {
+    // nothing goes here
+}
