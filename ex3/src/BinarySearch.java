5733de89bbf57f79b0a68b7d27beeeb2b0d7862b
GoryMoon <gurreja@gmail.com>
2018-11-21 11:57:27 +0100

    Fixed binarysearch

--- ex3/src/BinarySearch.java
+++ ex3/src/BinarySearch.java
@@ -1,41 +1,37 @@
-import java.util.Arrays;
-import java.util.Random;
-
 public class BinarySearch {
-  
-  /**
-   * requires: - 'array' is not null.
-   *           - the array is sorted by increasing value, i.e. for each index
-   *             'i' and index 'j', if 'i' < 'j' then 'array[i]' < 'array[j]'.
-   * ensures:  - if there is an index 'i' such that 'array[i] == value', the
-   *             method returns true.
-   *           - otherwise, it returns false.
-   *           - in either case, 'array' is left unchanged.
-   **/
-  public static boolean search(int[] array, int value) {
-    int left = 0;
-    int right = array.length - 1;
-    while (left <= right) {
-      int index = (right + left) / 2;
-      if (array[index] == value)
-        return true;
-      if (array[index] < value)
-        right = index - 1;
-      else
-        left = index + 1;
+
+    /**
+    * requires: - 'array' is not null.
+    *           - the array is sorted by increasing value, i.e. for each index
+    *             'i' and index 'j', if 'i' < 'j' then 'array[i]' < 'array[j]'.
+    * ensures:  - if there is an index 'i' such that 'array[i] == value', the
+    *             method returns true.
+    *           - otherwise, it returns false.
+    *           - in either case, 'array' is left unchanged.
+    **/
+    public static boolean search(int[] array, int value) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int index = (right + left) / 2;
+            if (array[index] == value)
+                return true;
+            if (array[index] > value)
+                right = index - 1;
+            else
+                left = index + 1;
+        }
+        return false;
     }
-    return false;
 
-  }
+    public static void main(String[] arg) {
+        int[] array = {1,2,3,4,5,6,7,8};
+        int value = 5 ;
 
-  public static void main(String[] arg) {
-    int[] array = {1,2,3,4,5,6,7,8};
-    int value = 5 ;
-
-    if (search(array,value))
-       System.out.println("The value " + value + " is in the array.");
-    else {
-       System.out.println("The value " + value + " is not in the array.");
-    }    
-  }
+        if (search(array,value))
+            System.out.println("The value " + value + " is in the array.");
+        else {
+            System.out.println("The value " + value + " is not in the array.");
+        }
+    }
 }
