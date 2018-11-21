ee0a56a39ce59dfcfecafa0b68f4241c7a89edd6
FarzadBT <farmerfaz@gmail.com>
2018-11-21 14:04:24 +0100

    Second version of report

--- ex2/test/InsertTests.java
+++ ex2/test/InsertTests.java
@@ -34,8 +34,7 @@ public class InsertTests {
         s.insert(1);
         s.insert(2);
         s.insert(3);
-        s.insert(1);
-        assertArrayEquals(s.toArray(), new int[]{1,1,2,3});
+        assertArrayEquals(s.toArray(), new int[]{1,2,3});
     }
 
     /**
